283f7d0ca4fc256475ea3f41b48ec2e9eae2fcd9
pckzy <sarun.debsirin@gmail.com>
2023-10-02 21:35:01 +0700

    built game screen

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 """PROJECT PSCP"""
-print("test push")
+# use pygame to create
+import pygame
+
+pygame.init()
+
+WIDTH, HEIGHT = 1200, 1010
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+surface = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
+tickrate = 50
+timer = pygame.time.Clock()
+
+run = True
+while run:
+    screen.fill('black') # change soon to picture
+    timer.tick(tickrate)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # exit game
+            run = False
+    pygame.display.flip()
+pygame.quit()
