c43d843f4f8120ffbbe34960e3c7890e6dccc2b4
pckzy <sarun.debsirin@gmail.com>
2023-10-02 21:49:06 +0700

    Added main menu tabs

--- main.py
+++ main.py
@@ -10,10 +10,24 @@ surface = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
 tickrate = 50
 timer = pygame.time.Clock()
 
+def draw_menu():
+    """main menu tabs"""
+    surface = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
+    # LEFT SIDE
+    pygame.draw.rect(surface, (0, 0, 0, 100), [70, 80, 600, 750], 0, 5)
+    pygame.draw.rect(surface, (0, 0, 0, 200), [70, 80, 600, 750], 5, 5)
+
+    # RIGHT SIDE
+    pygame.draw.rect(surface, (0, 0, 0, 100), [720, 80, 400, 750], 0, 5)
+    pygame.draw.rect(surface, (0, 0, 0, 200), [720, 80, 400, 750], 5, 5)
+
+    screen.blit(surface, (0, 0))
+
 run = True
 while run:
-    screen.fill('black') # change soon to picture
+    screen.fill('cornsilk4') # change soon to picture
     timer.tick(tickrate)
+    draw_menu()
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # exit game
             run = False
