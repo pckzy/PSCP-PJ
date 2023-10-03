b555ecae22eab01ed1b4ba1b2151ea627ac8669b
pckzy <sarun.debsirin@gmail.com>
2023-10-03 20:03:22 +0700

    Resize Game window

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ def draw_menu():
     """main menu tabs"""
     surface = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
     # LEFT SIDE
-    pygame.draw.rect(surface, (0, 0, 0, 100), [70, 80, 600, 750], 0, 5)
-    pygame.draw.rect(surface, (0, 0, 0, 200), [70, 80, 600, 750], 5, 5)
+    pygame.draw.rect(surface, (0, 0, 0, 100), [70, 70, 600, 590], 0, 5)
+    pygame.draw.rect(surface, (0, 0, 0, 200), [70, 70, 600, 590], 5, 5)
 
     # RIGHT SIDE
-    pygame.draw.rect(surface, (0, 0, 0, 100), [720, 80, 400, 750], 0, 5)
-    pygame.draw.rect(surface, (0, 0, 0, 200), [720, 80, 400, 750], 5, 5)
+    pygame.draw.rect(surface, (0, 0, 0, 100), [720, 70, 400, 590], 0, 5)
+    pygame.draw.rect(surface, (0, 0, 0, 200), [720, 70, 400, 590], 5, 5)
 
     screen.blit(surface, (0, 0))
 
