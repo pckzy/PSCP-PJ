667e42f41ae86b4cd5d3e51cbcb47760d5357de2
pckzy <sarun.debsirin@gmail.com>
2023-10-08 17:08:25 +0700

    Added clickable button into program

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ header_font = pygame.font.Font("resources/fonts/Square.ttf", 50)
 name_font = pygame.font.Font("resources/fonts/Square.ttf", 31)
 banner_font = pygame.font.Font("resources/fonts/1up.ttf", 28)
 mc_font = pygame.font.Font("resources/fonts/Minecrafter.Reg.ttf", 40)
+pause_font = pygame.font.Font('resources/fonts/1up.ttf', 38)
 
 # game variable
 score = 0
@@ -25,19 +26,48 @@ total_type = 0
 lives = 5 # default = 5
 level = 1
 active_string = ""
+paused = True
+
+class Button:
+    def __init__(self, x_pos, y_pos, text, clicked, surf):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.text = text
+        self.clicked = clicked
+        self.surf = surf
+
+    def draw(self):
+        btn_shape = pygame.draw.circle(self.surf, (0, 0, 0), (self.x_pos, self.y_pos), 35)
+        if btn_shape.collidepoint(pygame.mouse.get_pos()):
+            btn_event = pygame.mouse.get_pressed()
+            if btn_event[0]: # HOLD AND CLICK
+                pygame.draw.circle(self.surf, (190, 35, 35), (self.x_pos, self.y_pos), 35) # click
+                self.clicked = True
+            else: # HOLD BUT NOT CLICK
+                pygame.draw.circle(self.surf, (121, 120, 120), (self.x_pos, self.y_pos), 35) # hold
+        pygame.draw.circle(self.surf, 'white', (self.x_pos, self.y_pos), 35, 3)
+        self.surf.blit(pause_font.render(self.text, True, 'white'), (self.x_pos - 15, self.y_pos - 25))
 
 def draw_menu():
     """main menu tabs"""
     surface = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
     # LEFT SIDE
-    pygame.draw.rect(surface, (0, 0, 0, 100), [70, 70, 600, 590], 0, 5)
+    pygame.draw.rect(surface, (0, 0, 0, 50), [70, 70, 600, 590], 0, 5)
     pygame.draw.rect(surface, (0, 0, 0, 200), [70, 70, 600, 590], 5, 5)
+    surface.blit(header_font.render('MENU :', True, 'black'), (95, 90))
+    btn_resume = Button(125, 190, '>', False, surface)
+    surface.blit(header_font.render('PLAY!', True, 'white'), (175, 165))
+    btn_quit = Button(375, 190, 'X', False, surface)
+    surface.blit(header_font.render('QUIT!', True, 'white'), (425, 165))
+    btn_resume.draw()
+    btn_quit.draw()
 
     # RIGHT SIDE
-    pygame.draw.rect(surface, (0, 0, 0, 100), [720, 70, 400, 590], 0, 5)
+    pygame.draw.rect(surface, (0, 0, 0, 50), [720, 70, 400, 590], 0, 5)
     pygame.draw.rect(surface, (0, 0, 0, 200), [720, 70, 400, 590], 5, 5)
 
     screen.blit(surface, (0, 0))
+    return btn_resume.clicked, btn_quit.clicked
 
 def draw_screen():
     # screen border
@@ -58,14 +88,27 @@ def draw_screen():
     screen.blit(header_font.render(f'"{active_string}"', True, 'white'), (280, HEIGHT - 75))
     screen.blit(mc_font.render(f'PSCP-PROJECT', True, 'white'), (845, HEIGHT - 67))
 
+    # button
+    pause_btn = Button(778, HEIGHT - 52, 'II', False, screen)
+    pause_btn.draw()
+    return pause_btn.clicked
+
 run = True
 while run:
     screen.blit(background, (0, 0))
     timer.tick(tickrate)
-    draw_screen()
-    draw_menu()
+    stop_btn = draw_screen()
+    if paused == True:
+        draw_menu()
+        resume_btn, quit_btn = draw_menu()
+        if resume_btn:
+            paused = False
+        if quit_btn:
+            run = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # exit game
             run = False
+    if stop_btn:
+        paused = True
     pygame.display.flip()
 pygame.quit()
